6c1e314a5b1e96b62e916f3e36ba6125ab9fd068
Anleu4 <josueanleu4@gmail.com>
2024-05-09 09:47:29 -0600

    Creacion de footer

---
+++ footer/footer.js
@@ -0,0 +1,23 @@
+let footer=  document.querySelector("footer")
+footer.innerHTML=`
+<div class="correos">
+    <p>correos:</p>
+    <p>jbanleu@scl.edu.gt</p>
+    <p>jdaceituno@scl.edu.gt</p>
+</div>
+<div class="numeros">
+<p>Numero:</p>
+<p>+502 1234-5678</p>
+<img class="fb" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/facebook-svgrepo-com.svg" alt="">
+<img class="ig" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/instagram-svgrepo-com.svg" alt="">
+<img class="tw" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/twitter-svgrepo-com.svg" alt="">
+</div>
+<div class="Ofrecemos">
+    <p class="Ofrecemos2">Ofrecemos:</p>
+    <p>pantalones</p>
+    <p>Camisas</p>
+    <p>Camisetas</p>
+    <p>Calcetines</p>
+
+</div>
+`	
