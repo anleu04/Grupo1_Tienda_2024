ded0f297a2f58f8c636bf90c7547e43cfef5dfd6
DAVID0205J <jdaceituno@scl.edu.gt>
2024-05-12 19:12:01 -0600

    diseno de footer mejorado

--- footer/footer.js
+++ footer/footer.js
@@ -1,23 +1,45 @@
 let footer=  document.querySelector("footer")
 footer.innerHTML=`
+<div class="descTienda">
+    <h3 class="desc_F">Description</h3>
+    <p class="T_desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem incidunt nam fugit, quam eum rerum deleniti ducimus nemo sequi quisquam ut soluta explicabo dolor dolores? Libero vel molestiae vero voluptates.</p>
+</div>
 <div class="correos">
-    <p>correos:</p>
-    <p>jbanleu@scl.edu.gt</p>
-    <p>jdaceituno@scl.edu.gt</p>
+    <h3 class="correo">Contact Us</h3>
+    <p class="C1">jbanleu@scl.edu.gt</p>
+    <p class="C2">jdaceituno@scl.edu.gt</p>
+    <h3 class="T_phone">phone</h3>
+    <p class="phone">+502 1234-5678</p>
+
+    
+    <div class="Apps">
+        <img class="fb" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/facebook-svgrepo-com.svg" alt="">
+        <img class="ig" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/instagram-svgrepo-com.svg" alt="">
+        <img class="tw" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/twitter-svgrepo-com.svg" alt="">
+        </div>
 </div>
-<div class="numeros">
-<p>Numero:</p>
-<p>+502 1234-5678</p>
-<img class="fb" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/facebook-svgrepo-com.svg" alt="">
-<img class="ig" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/instagram-svgrepo-com.svg" alt="">
-<img class="tw" src="https://raw.githubusercontent.com/EduardoAjcajon/Img_Tienda/0a27608117ae4782255a086b8f97f46f0be09c1d/twitter-svgrepo-com.svg" alt="">
-</div>
-<div class="Ofrecemos">
-    <p class="Ofrecemos2">Ofrecemos:</p>
+
+<div class="Shop">
+    <h3 class="h_shop">Shop</h3>
+
+    <div class="ropa">
+    
     <p>pantalones</p>
     <p>Camisas</p>
     <p>Camisetas</p>
     <p>Calcetines</p>
+    </div>
 
+</div>
+
+<div class="opening">
+
+        <h3>Address</h3>
+        <p>8911 Tanglewood Ave.
+            Capitol Heights,MD 20743</p>
+
+        <h3>Opening Hours</h3>
+        <p>Mon-Fri 08:00AM - 08:00PM</p>
+        <p>Sat-Sun 10:00AM - 06:00PM</p>
 </div>
 `	
